9094e46d4bf2517a82cb90996a2aa58e7342bbbf
laa66 <max.sclz94@gmail.com>
2023-03-28 20:36:29 +0200

    Configured React-app to send POST requests with CSRF token

--- react-app/src/components/logOut/logOut.js
+++ react-app/src/components/logOut/logOut.js
@@ -1,3 +1,13 @@
+//eslint-disable-next-line
+const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+
 export const logOut = () => {
     localStorage.clear();
+    fetch('http://localhost:8080/logout', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+        'X-XSRF-TOKEN': csrfToken
+    }
+    }).then((response) => response.json());
 }
--- react-app/src/components/track/postTrack.js
+++ react-app/src/components/track/postTrack.js
@@ -1,14 +1,26 @@
+//eslint-disable-next-line
+const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+
 export const postTrackShort = () =>  fetch('http://localhost:8080/api/playlist/create?range=short', {
     method: 'POST',
-    credentials: 'include'
+    credentials: 'include',
+    headers: {
+        'X-XSRF-TOKEN': csrfToken
+    }
 }).then((response) => response.json());
 
 export const postTrackMedium = () =>  fetch('http://localhost:8080/api/playlist/create?range=medium', {
     method: 'POST',
-    credentials: 'include'
+    credentials: 'include',
+    headers: {
+        'X-XSRF-TOKEN': csrfToken
+    }
 }).then((response) => response.json());
 
 export const postTrackLong = () =>  fetch('http://localhost:8080/api/playlist/create?range=long', {
     method: 'POST',
-    credentials: 'include'
+    credentials: 'include',
+    headers: {
+        'X-XSRF-TOKEN': csrfToken
+    }
 }).then((response) => response.json());
