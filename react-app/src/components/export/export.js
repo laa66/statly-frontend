809ff1e685f59b2dfd2899b54e3441f2a57145f2
laa66 <max.sclz94@gmail.com>
2023-03-31 18:18:30 +0200

    Added playlist preview button to TopTracks section

--- react-app/src/components/export/export.js
+++ react-app/src/components/export/export.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './export.css';
 
 function Export({postTrack}) {
 
+    const [showButton, setShowButton] = useState(true);
+    const [playlist, setPlaylist] = useState([]);
+
+    const toggleButton = () => {
+        setShowButton(!showButton);
+        postTrack().then((data) => {
+            setPlaylist(data);
+        }).catch((err) =>{console.log(err.message)});
+    }
+
     return (
         <div className="container export-section">
+            {!showButton && <div style={{color:"#535353"}}>Playlist created and exported to your <span style={{color:"#1db954"}}>Spotify account!</span></div>}
             <div className="export-button">
-                <div className="nav-range-item" style={{width:"20%", height:"90%"}} onClick={postTrack}>Create playlist</div>
+                {showButton && <div className="nav-range-item" style={{width:"20%", height:"90%"}} onClick={toggleButton}>Create playlist</div>}
+                {!showButton && <div className="check-playlist-button" style={{width:"20%", height:"90%", fontWeight:"400"}}
+                onClick={() => {window.location.href=playlist.external_urls.spotify}}>Check playlist</div>}
             </div>
             <div>
                 <p>Take your music with you wherever you go.</p> 
