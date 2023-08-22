7f49935c0fd647f2912cc32e5a7344e91536f854
laa66 <max.sclz94@gmail.com>
2023-08-22 21:33:41 +0200

    Modified LocationMap, added lines between users on earth map

--- src/components/location/LocationMap.js
+++ src/components/location/LocationMap.js
@@ -1,23 +1,12 @@
-import * as React from 'react';
 import Map from 'react-map-gl';
 import { Layer, Source, Marker } from 'react-map-gl';
 
+import basemarkericon from '../../resources/basemarkericon.png';
 import markericon from '../../resources/markericon.png';
 
 function LocationMap({data}) {
-    
-    /*const geojson = {
-        'type': 'Feature',
-        'properties': {},
-        'geometry': {
-            'type': 'line',
-            'coordinates': [
-                [-122.483696, 37.833818],
-                [0, 37.833683]
-            ]
-        }
-    };
 
+    const coordinates = [];
     const layerStyle = {
         'id': 'route',
         'type': 'line',
@@ -27,46 +16,45 @@ function LocationMap({data}) {
             'line-cap': 'round'
         },
         'paint': {
-            'line-color': '#1db954',
-            'line-width': 3
+            'line-color': '#00A573',
+            'line-width': 2
         }
-    };*/
+    };
     
     return (
-        /*<>
-        {data.length > 0 && <Map
-                style={{width:'940px', height:'600px'}}
-                initialViewState={{zoom:-5}}
-                mapboxAccessToken="{token}"
-                mapStyle="mapbox://styles/mapbox/dark-v11">
-                <Marker longitude={data[0].coordinates.longitude} latitude={data[0].coordinates.latitude} anchor="bottom" />
-                
-                <Source id="data" type="geojson" data={geojson}>
-                    <Layer {...layerStyle} />
-                </Source>
-            </Map> }
-        </>*/
         <>
             {data.length > 0 && <Map
                 style={{ width: '940px', height: '600px' }}
-                initialViewState={{ zoom: 1.5 }}
-                mapboxAccessToken="{token}"
+                initialViewState={{ zoom: 1.5, longitude: data[0]?.coordinates.longitude, latitude: data[0]?.coordinates.latitude}}
+                mapboxAccessToken="pk.eyJ1IjoibGFhNjYiLCJhIjoiY2xrc2pudDFuMDA4cTNnbnBlMHM5YW0wNSJ9.zt1xEp0w7pgucF75qiqE3A"
                 mapStyle="mapbox://styles/mapbox/dark-v11">
                 <Marker longitude={data[0]?.coordinates.longitude} latitude={data[0]?.coordinates.latitude}>
-                    <img src={markericon} alt="marker-icon" width={'40px'} height={'40px'} />
+                    <img src={basemarkericon} alt="marker-icon" width={'40px'} height={'40px'} />
                 </Marker>
                 {
                     data.slice(1).map((d, i) => {
                         return (
-                            <Marker key={d + i} longitude={d.coordinates.longitude} latitude={d.coordinates.latitude}>
-                                <img src={markericon} alt="marker-icon" width={'40px'} height={'40px'} />
-                            </Marker>
+                            <div key={i}>
+                                <Marker longitude={d.coordinates.longitude} latitude={d.coordinates.latitude}>
+                                    <img src={markericon} alt="marker-icon" width={'40px'} height={'40px'} />
+                                </Marker>
+                                {coordinates.push([data[0]?.coordinates.longitude, data[0]?.coordinates.latitude])}
+                                {coordinates.push([d.coordinates.longitude, d.coordinates.latitude])}
+                            </div>
                         );
                     })
                 }
-
+                <Source id="data" type="geojson" data={{
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': coordinates
+                    }
+                }}>
+                    <Layer {...layerStyle} />
+                </Source>
             </Map>
-
             }
         </>
     );
