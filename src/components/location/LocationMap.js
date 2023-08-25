cd90f734571a5c1b59792829e862ee7c931e33ff
laa66 <max.sclz94@gmail.com>
2023-08-25 18:59:43 +0200

    Created reactive Popup in LocationMap component, deleted matchLocation.css

--- src/components/location/LocationMap.js
+++ src/components/location/LocationMap.js
@@ -1,11 +1,13 @@
-import Map from 'react-map-gl';
+import Map, { Popup } from 'react-map-gl';
 import { Layer, Source, Marker } from 'react-map-gl';
 
 import basemarkericon from '../../resources/basemarkericon.png';
 import markericon from '../../resources/markericon.png';
+import { useState } from 'react';
 
 function LocationMap({data}) {
-
+    
+    const [user, setUser] = useState(null);
     const coordinates = [];
     const layerStyle = {
         'id': 'route',
@@ -24,7 +26,7 @@ function LocationMap({data}) {
     return (
         <>
             {data.length > 0 && <Map
-                style={{ width: '940px', height: '600px' }}
+                style={{ width: '1450px', height: '700px' }}
                 initialViewState={{ zoom: 1.5, longitude: data[0]?.coordinates.longitude, latitude: data[0]?.coordinates.latitude}}
                 mapboxAccessToken="pk.eyJ1IjoibGFhNjYiLCJhIjoiY2xrc2pudDFuMDA4cTNnbnBlMHM5YW0wNSJ9.zt1xEp0w7pgucF75qiqE3A"
                 mapStyle="mapbox://styles/mapbox/dark-v11">
@@ -32,18 +34,51 @@ function LocationMap({data}) {
                     <img src={basemarkericon} alt="marker-icon" width={'40px'} height={'40px'} />
                 </Marker>
                 {
-                    data.slice(1).map((d, i) => {
+                    data.slice(1).map((user, i) => {
                         return (
                             <div key={i}>
-                                <Marker longitude={d.coordinates.longitude} latitude={d.coordinates.latitude}>
+                                <Marker
+                                longitude={user.coordinates.longitude} 
+                                latitude={user.coordinates.latitude}
+                                onClick={e => {
+                                    e.originalEvent.stopPropagation();
+                                    setUser(user);
+                                }}>
                                     <img src={markericon} alt="marker-icon" width={'40px'} height={'40px'} />
                                 </Marker>
                                 {coordinates.push([data[0]?.coordinates.longitude, data[0]?.coordinates.latitude])}
-                                {coordinates.push([d.coordinates.longitude, d.coordinates.latitude])}
+                                {coordinates.push([user.coordinates.longitude, user.coordinates.latitude])}
                             </div>
                         );
                     })
                 }
+                {user && 
+                <Popup
+                anchor="bottom"
+                longitude={user.coordinates.longitude}
+                latitude={user.coordinates.latitude}
+                onClose={() => setUser(null)}
+                >
+                <div className="map-popup">
+                    <div>
+                        <div className="row gx-0">
+                            <div className="col-3 map-popup-img">
+                                <img src={user.images[0].url} alt="test" width={'30px'} height={'30px'}/>
+                            </div>
+                            <div className="col-9 map-popup-title">
+                                {user.display_name}
+                            </div>
+                            <div className="map-popup-line">{user.location.features[0].text}, {user.location.features[1].text}</div>
+                        <div className="map-popup-line">Match - {user.match}%</div>
+                        <div className="map-popup-line">Distance - {parseInt(user.distance * 0.001)}km</div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                </Popup>
+                }
+
                 <Source id="data" type="geojson" data={{
                     'type': 'Feature',
                     'properties': {},
--- src/components/location/matchLocation.js
+++ src/components/location/matchLocation.js
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import './matchLocation.css';
 import { GetRequest } from '../request/apiUrl';
 import { getRequest } from '../request/getRequest';
 import { useState } from 'react';
@@ -19,58 +18,15 @@ function MatchLocation() {
             setStatus(err.message);
         });
     // eslint-disable-next-line
-    }, []);
+    }, []);0
 
     if (hasError) return (<div><Error code={status}/></div>);
     return (
         <div className="location-container animate-fade">
             <div className="image-description">
-                <h1>Your music taste matches</h1>
-                <div style={{ color: "#7d7d7d" }}>Connect with like-minded individuals who share your musical vibe</div>
-                <div className="row mt-5">
-                    <div className="col-xl-2"> 
-                        {users?.slice(1, 6).map((data, i) => {
-                            return (
-                                <div key={data + i} className="match-user-container row mb-3">
-                                    <div className="col-2 d-flex align-items-center justify-content-center">
-                                        <img className="friend-image" src={data.images[0].url} alt="test" width={'40px'} height={'40px'} />
-                                    </div>
-                                    <div className="col-10 text-start">
-                                        <div className="match-user-nick match-text-overflow">{data?.display_name}</div>
-                                        <div className="match-text-overflow" style={{ color: "#7d7d7d" }}>{data.location.features[0].text}, {data.location.features[1].text}</div>
-                                        <div className="match-user-stats match-text-overflow">
-                                            <span style={{ color: "#1db954" }}>{data.match}%</span>
-                                            <span style={{ color: "#121212" }}> | </span>
-                                            <span style={{ color: "#00A573" }}>{parseInt(data.distance * 0.001)} km</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div className="col-xl-8">
-                        <Map data={users}/>
-                    </div>
-                    <div className="col-xl-2">
-                        {users?.slice(6).map((data, i) => {
-                            return (
-                                <div key={data + i} className="match-user-container row mb-3">
-                                    <div className="col-2 d-flex align-items-center justify-content-center">
-                                        <img className="friend-image" src={data.images[0].url} alt="test" width={'40px'} height={'40px'} />
-                                    </div>
-                                    <div className="col-10 text-start">
-                                        <div className="match-user-nick match-text-overflow">{data?.display_name}</div>
-                                        <div className="match-text-overflow" style={{ color: "#7d7d7d" }}>{data.location.features[0].text}, {data.location.features[1].text}</div>
-                                        <div className="match-user-stats match-text-overflow">
-                                            <span style={{ color: "#1db954" }}>{data.match}%</span>
-                                            <span style={{ color: "#121212" }}> | </span>
-                                            <span style={{ color: "#00A573" }}>{parseInt(data.distance * 0.001)} km</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
+                <h4>Connect with like-minded individuals who share your musical vibe</h4>
+                <div className="mt-4">
+                    <Map data={users}/>
                 </div>
             </div>
         </div>
